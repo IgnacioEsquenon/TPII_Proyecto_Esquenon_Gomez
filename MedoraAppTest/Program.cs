c6efa405d7b6f4978a83aed62938c75e643f489a
Ignacio Esquenón <esquenonn@gmail.com>
2025-09-10 23:20:11 -0300

    Commit #03

--- MedoraAppTest/Program.cs
+++ MedoraAppTest/Program.cs
@@ -20,7 +20,7 @@ namespace MedoraAppTest
                     HoraInicio = new TimeSpan(7, 0, 0),   // 7:00 AM
                     HoraFin = new TimeSpan(9, 0, 0),      // 9:00 AM
                     DuracionTurnos = 30,                  // Turnos de 30 minutos
-                    IdUsuario = 2,                        // ID del médico en tabla Usuario
+                    IdUsuario = 5,                        // ID del médico en tabla Usuario
                     IdDia = 2                              // 1=lunes, 2=martes...
                 };
 
